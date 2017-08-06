fcfebbef990e78cc7486695f52198ca1ba44d6fe
hoqqun <hoqqun@gmail.com>
2017-08-06 11:35:39 +0900

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 Hiari.create!([
-  {latitude: 34.6905685985705, longitude: 135.206772699414, comment: "神戸港にてヒアリが発見された。", image: nil, discover_ari_date: "2017-06-22 01:18:00", title: "日本発、発見", address: "日本、〒651-0082 兵庫県神戸市中央区小野浜町７−３０ 国土交通省近畿地方整備局", user_id: nil, ipaddress: "10.0.2.2"}
+  {latitude: 34.6905685985705, longitude: 135.206772699414, comment: "神戸港にてヒアリが発見された。", image: nil, discover_ari_date: "2017-06-21 16:18:00", title: "日本発、発見", address: "日本、〒651-0082 兵庫県神戸市中央区小野浜町７−３０ 国土交通省近畿地方整備局", user_id: nil, ipaddress: "10.0.2.2"},
+  {latitude: 35.0204592, longitude: 136.792326, comment: "ヒアリ7匹発見される。", image: nil, discover_ari_date: "2017-06-30 00:00:00", title: "名古屋港鍋田ふ頭のコンテナターミナル", address: "日本、〒498-0069 愛知県弥富市富浜５丁目１ 名古屋ユナイテッドコンテナターミナル", user_id: nil, ipaddress: "127.0.0.1"},
+  {latitude: 35.5946729, longitude: 139.7527161, comment: "", image: nil, discover_ari_date: "2017-07-21 02:29:00", title: "大井ふ頭でヒアリ発見される", address: "日本、〒140-0003 東京都品川区八潮４丁目１ 南部公園管理（事）施設整備係", user_id: nil, ipaddress: "127.0.0.1"},
+  {latitude: 35.247611, longitude: 136.9722236, comment: "ヒアリ発見される！。\r\nhttp://www.nikkei.com/article/DGXLASFD12H6K_T10C17A7CN0000/", image: nil, discover_ari_date: "2017-07-21 02:29:00", title: "春日井市の内陸部", address: "日本、〒486-0844 愛知県春日井市鳥居松町５丁目 春日井市役所", user_id: nil, ipaddress: "127.0.0.1"},
+  {latitude: 35.4453829813067, longitude: 139.670715115283, comment: "おしゃれな横浜で！要注意。", image: nil, discover_ari_date: "2017-07-21 02:31:00", title: "横浜港ヒアリ発見！", address: "日本、〒231-0811 神奈川県横浜市中区本牧ふ頭９", user_id: nil, ipaddress: "127.0.0.1"},
+  {latitude: 33.6642965, longitude: 130.4209817, comment: "", image: nil, discover_ari_date: "2017-07-21 02:32:00", title: "東区アイランドシティ", address: "日本、〒813-0017 福岡県福岡市東区香椎照葉３丁目２−１", user_id: nil, ipaddress: "127.0.0.1"}
 ])
